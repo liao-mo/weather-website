1d7b30898a7b7c2e2ac0f196db5c1fff9f3b789b
MOCHE, LIAO <pppoooppp654852@gmail.com>
2023-07-08 14:40:26 +0800

    =remove some console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('loading client side javascript file...')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
